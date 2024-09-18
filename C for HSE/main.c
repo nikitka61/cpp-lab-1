c8117aabcfeb2c43a6ddc0a45b88bc2551bb34fc
nikitka61 <ninitarasov@edu.hse.ru>
2024-09-18 21:30:40 +0300

    Initial Commit

---
+++ C for HSE/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C for HSE
+//
+//  Created by Никита  on 18.09.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
